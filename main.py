d40071ba43ef8b46a6ad1533f57e25cda73d2e6d
stingray0225 <114566779+ErrnoUnknown@users.noreply.github.com>
2024-03-23 14:37:14 +0900

    흰 건반 추가

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ import streamlit as st
 from pydub import AudioSegment
 from pydub.playback import play
 
-# Def
+# 상수 정의
+PIANO_KEYS = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+
+# 함수 정의
 def play_wav(path):
     song = AudioSegment.from_wav(path)
     play(song)  
 
+st.markdown(
+    """
+<style>
+button {
+    height: 100px;
+    padding-top: 10px !important;
+    padding-bottom: 10px !important;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
 # 테스트용 버튼
-if st.button('audio test A'):
-    # 버튼이 눌렸을 때 소리 재생
-    play_wav('static/sound/piano_a.wav')
-
-    # 테스트용 버튼
-if st.button('audio test B'):
-    # 버튼이 눌렸을 때 소리 재생
-    play_wav('static/sound/piano_b.wav')
-
-
-
-
+for key in PIANO_KEYS:
+    if st.button(key):
+        # 버튼이 눌렸을 때 소리 재생
+        play_wav(f'static/sound/piano_{key.lower()}.wav')
