0afb20a5bbe055ff4ff95165b094c6360842bd45
stingray0225 <114566779+ErrnoUnknown@users.noreply.github.com>
2024-03-23 14:03:20 +0900

    테스트 코드 업로드

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+# Import
 import streamlit as st
-import winsound
+from pydub import AudioSegment
+from pydub.playback import play
 
-if st.button('Say hello'):
-    winsound.Beep(523, 500)
+# Def
+def play_wav(path):
+    song = AudioSegment.from_wav(path)
+    play(song)  
+
+# 테스트용 버튼
+if st.button('audio test A'):
+    # 버튼이 눌렸을 때 소리 재생
+    play_wav('static/sound/piano_a.wav')
+
+    # 테스트용 버튼
+if st.button('audio test B'):
+    # 버튼이 눌렸을 때 소리 재생
+    play_wav('static/sound/piano_b.wav')
+
+
+
+
