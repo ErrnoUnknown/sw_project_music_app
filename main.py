44e89e15966e748bc7e99c53dc6ff1c2653acdb6
stingray0225 <114566779+ErrnoUnknown@users.noreply.github.com>
2024-03-30 14:48:36 +0900

    Update

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
+# streamlit run c:\Users\DICCE\Desktop\sw_project_music_app\main.py
+
 # Import
 import streamlit as st
 from pydub import AudioSegment
 from pydub.playback import play
+import mido
 
 # 상수 정의
-PIANO_WHITE_KEYS = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
-PIANO_BLACK_KEYS = ['C#', 'D#', 'F#', 'G#', 'A#']
+PIANO_KEYS = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+KEY_TO_NOTE = {}
 
-STYLE = '''<style>
-button {
-    height: 20px;
-    width: 30px
-    padding-top: 10px !important;
-    padding-bottom: 10px !important;
-    background-color: rgb(0, 0, 0);
-    opacity: 0.8;
-    display:inline-block;
-}
-</style>'''
+for index in range(12):
+    KEY_TO_NOTE[PIANO_KEYS[index]] = index + 60
 
 # 함수 정의
 def play_mp3(path):
     mp3 = AudioSegment.from_mp3(path)
-    play(mp3)  
+    play(mp3)
 
-st.markdown(STYLE, unsafe_allow_html=True,)
+def record_note(note, time):
+    global music_track
 
-# 테스트용 버튼
-for key in PIANO_WHITE_KEYS:
+    music_track.append(
+        'note_on',
+        note=note,
+        velocity=64,
+        time=time
+    )
+
+    music_track.append(
+        'note_off',
+        note=note,
+        velocity=64,
+        time=time + 0.25
+    )
+
+# 메인
+mid = mido.MidiFile()
+music_track = mido.MidiTrack()
+mid.tracks.append(music_track)
+
+for key in PIANO_KEYS:
     if st.button(key):
         # 버튼이 눌렸을 때 소리 재생
         play_mp3(f'static/sound/piano_{key.lower()}.wav')
-
-for key in PIANO_BLACK_KEYS:
-    if st.button(key):
-        # 버튼이 눌렸을 때 소리 재생
-        play_mp3(f'static/sound/piano_{key.lower()}.wav')
+        record_note(record_note(KEY_TO_NOTE), 0)
